761adcfe67fc38ada61a954cb5ca699488afbe12
abmo72 <86527620+abmo72@users.noreply.github.com>
2021-08-20 22:59:46 +0430

    Update jqery.js

--- js/jqery.js
+++ js/jqery.js
@@ -17,40 +17,51 @@ $(document).ready(function(){
     // start od code for edite information data
 
     $(document).on('click','#dell',function(){
-    $(this).parent().parent().remove();
-})
-    $("#table").on('click','#edit',function(){
-    $("form").append("<button type='button' id='update'>"+"update"+"</button>");
-    var currentRow=$(this).closest("tr");
-    var col1=currentRow.find("td:eq(0)").text();
-    var col2=currentRow.find("td:eq(1)").text();
-    var col3=currentRow.find("td:eq(2)").text();
-    var col4=currentRow.find("td:eq(3)").text();
-    $("#name").val(col1);
-    $("#email").val(col2);
-    $("#phone").val(col3);
-    $("#web").val(col4);
+        $(this).parent().parent().remove();
+    })
+    $("#table").on('click','#edit',function() {
+        $("form").append("<button type='button' id='update'>"+"update"+"</button>");
+        var currentRow = $(this).closest("tr");
+        var col1 = currentRow.find("td:eq(0)").text();
+        var col2 = currentRow.find("td:eq(1)").text();
+        var col3 = currentRow.find("td:eq(2)").text();
+        var col4 = currentRow.find("td:eq(3)").text();
+        $("#name").val(col1);
+        $("#email").val(col2);
+        $("#phone").val(col3);
+        $("#web").val(col4);
+        $("#save").css("display", "none");
+        $("#update").css("display", "block");
+        $("#table").find(".updating").removeClass("updating");
+        currentRow.addClass("updating");
 
 // codes of update button
 
+
     $("#form").on('click','#update',function(){
-    var new_row=  "<tr>" +"<td>" +$('#name').val()+ "</td>"
-    +"<td >" + $('#email').val()+"</td>"
-    +"<td>" +$('#phone').val()+"</td>"
-    +"<td>" +$('#web').val()+"</td>"
-    + "<td>" + "<button type='button' id='dell'>"+"Remove"+"</button>"
-    + "</td>" + "<td >" +
-    "<button type='button' id='edit'>"+"edit"+"</button>" + "</td>"+"</tr>"
-        $("#table").find($(currentRow)).replaceWith(new_row);
-        $("table tbody").find($(currentRow)).replaceWith(new_row);
+        // $("#save").removeClass("hide");
+        // $("#update").addClaas("hide");
+        var new_row=  "<tr>" +"<td>" +$('#name').val()+ "</td>"
+            +"<td >" + $('#email').val()+"</td>"
+            +"<td>" +$('#phone').val()+"</td>"
+            +"<td>" +$('#web').val()+"</td>"
+            + "<td>" + "<button type='button' id='dell'>"+"Remove"+"</button>"
+            + "</td>" + "<td >" +
+            "<button type='button' id='edit'>"+"edit"+"</button>" + "</td>"+"</tr>"
+        $("#table").find(".updating").replaceWith(new_row);
+
+
         if(currentRow){
-    $("table tbody").find($(currentRow)).replaceWith(new_row);
-    currentRow = null;
-}
-    $("#update").remove();
+            $("table tbody").find(".updating").replaceWith(new_row);
+            currentRow = null;
+        }
+        $("#update").css("display","none");
+        $("#save").css("display","block");
+    });
 
 });
 
-});
 
 });
+
+
